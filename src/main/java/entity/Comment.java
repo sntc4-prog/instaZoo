7dea6c5f1e3f543574c946819a480eaebe3ba9c2
Egor <egorborisovzypo@gmail.com>
2022-01-24 18:22:38 +0600

    Updated Comment model

--- src/main/java/entity/Comment.java
+++ src/main/java/entity/Comment.java
@@ -1,4 +1,18 @@
 package entity;
 
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+
 public class Comment {
+    private Long id;
+    private Post post;
+    private String username;
+    private Long userId;
+    private String message;
+    private LocalDateTime createdDate;
+
+    @PrePersist //Изначальное назначение текущего времени до создания
+    protected void OnCreate() {
+        this.createdDate = LocalDateTime.now();
+    }
 }
