6f3f20ab8e3c3290e2d190fdf8f02bc0d86d8f57
Egor <egorborisovzypo@gmail.com>
2022-01-24 22:11:17 +0600

    Added CustomUserDetailsService

--- src/main/java/ru/borisov/instazoo/entity/User.java
+++ src/main/java/ru/borisov/instazoo/entity/User.java
@@ -1,6 +1,7 @@
 package ru.borisov.instazoo.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.security.core.userdetails.UserDetails;
 import ru.borisov.instazoo.entity.enums.ERole;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,7 +12,7 @@ import java.util.*;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -39,9 +40,46 @@ public class User {
     @Transient
     private Collection<? extends GrantedAuthority> authorities;
 
+    public User() {
+    }
+
+    public User(Long id, String username, String password, String email, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.authorities = authorities;
+    }
+
     @PrePersist //Изначальное назначение текущего времени до создания
     protected void OnCreate() {
         this.createdDate = LocalDateTime.now();
     }
 
+    /*Spring Security methods implemented*/
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/ru/borisov/instazoo/service/CustomUserDetailsService.java
@@ -0,0 +1,44 @@
+package ru.borisov.instazoo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.borisov.instazoo.entity.User;
+import ru.borisov.instazoo.repository.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with username: " + username))        ;
+        return build(user);
+    }
+    public User loadUserById(Long id){
+        return userRepository.findUserById(id).orElse(null);
+    }
+
+    public static User build(User user){
+        List<GrantedAuthority> authorities = user.getRole().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+        return new User(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
+}
