38a5814c3f06698ffd4a7080a6c3ca53cf3e93eb
Egor <egorborisovzypo@gmail.com>
2022-01-25 19:57:22 +0600

    Added AuthController and added ResponseErrorValidation in validations

---
+++ src/main/java/ru/borisov/instazoo/web/AuthController.java
@@ -0,0 +1,13 @@
+package ru.borisov.instazoo.web;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/auth")
+@PreAuthorize("permitAll()")
+public class AuthController {
+}
