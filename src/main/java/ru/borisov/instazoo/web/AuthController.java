f3e2b03dfe4c59575391557abaafe173e6ea3886
Egor <egorborisovzypo@gmail.com>
2022-01-25 20:17:42 +0600

    Updated AuthController, Added JWTTokenSuccessResponse and MessageResponse

--- src/main/java/ru/borisov/instazoo/web/AuthController.java
+++ src/main/java/ru/borisov/instazoo/web/AuthController.java
@@ -1,13 +1,60 @@
 package ru.borisov.instazoo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.borisov.instazoo.payload.request.LoginRequest;
+import ru.borisov.instazoo.payload.request.SignupRequest;
+import ru.borisov.instazoo.payload.response.JWTTokenSuccessResponse;
+import ru.borisov.instazoo.payload.response.MessageResponse;
+import ru.borisov.instazoo.security.JWTTokenProvider;
+import ru.borisov.instazoo.security.SecurityConstants;
+import ru.borisov.instazoo.service.UserService;
+import ru.borisov.instazoo.validations.ResponseErrorValidation;
+
+import javax.validation.Valid;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/auth")
 @PreAuthorize("permitAll()")
 public class AuthController {
+    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private ResponseErrorValidation responseErrorValidation;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<Object> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult bindingResult) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (ObjectUtils.isEmpty(errors)) return errors;
+
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(),
+                loginRequest.getPassword()
+        ));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = SecurityConstants.TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JWTTokenSuccessResponse(true, jwt));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> registerUser(@Valid @RequestBody SignupRequest signupRequest, BindingResult bindingResult) {
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (ObjectUtils.isEmpty(errors)) return errors;
+
+        userService.createUser(signupRequest);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+    }
 }
