285a00fa8d5118194a2affe19ea67215f416662a
Egor <egorborisovzypo@gmail.com>
2022-01-25 12:42:58 +0600

    Updated SecurityConstants - TOKEN_PREFIX

--- src/main/java/ru/borisov/instazoo/security/SecurityConstants.java
+++ src/main/java/ru/borisov/instazoo/security/SecurityConstants.java
@@ -5,7 +5,7 @@ public class SecurityConstants {
     public static final String SIGN_UP_URLS = "/api/auth/**";
 
     public static final String SECRET = "SecretKeyGenJWT";
-    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String CONTENT_TYPE = "application/json";
     public static final long EXPIRATION_TIME = 600_000; //10min
